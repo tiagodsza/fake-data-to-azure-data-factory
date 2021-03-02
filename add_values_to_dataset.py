9c3d0140b45fea55aaf85e05761161f72c0043f7
tiago.souza <tiagodsza@gmail.com>
2021-03-02 15:52:10 -0300

    Commit

--- add_values_to_dataset.py
+++ add_values_to_dataset.py
@@ -2,12 +2,14 @@ import csv
 import datetime
 from time import sleep
 import random
+import os
+
 FIELD_NAMES = ['date', 'value']
 
-
 while True:
-    sleep(300)
     with open('dataset-date-value.csv', 'a', newline='') as csv_file:
         dict_writer = csv.DictWriter(csv_file, fieldnames=FIELD_NAMES)
         row = {'date': datetime.datetime.now(), 'value': random.randint(1, 1000)}
         dict_writer.writerow(row)
+        os.system('sh add-git.sh')
+    sleep(300)
