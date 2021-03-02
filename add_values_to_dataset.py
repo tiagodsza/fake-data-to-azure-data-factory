f46f0949bbe4c238007ade38b41250d18f28f212
tiago.souza <tiagodsza@gmail.com>
2021-03-02 15:54:23 -0300

    Commit

--- add_values_to_dataset.py
+++ add_values_to_dataset.py
@@ -11,5 +11,7 @@ while True:
         dict_writer = csv.DictWriter(csv_file, fieldnames=FIELD_NAMES)
         row = {'date': datetime.datetime.now(), 'value': random.randint(1, 1000)}
         dict_writer.writerow(row)
-        os.system('sh add-git.sh')
+        os.system('git add --all')
+        os.system('git commit -am "Commit"')
+        os.system('git push origin master')
     sleep(300)
